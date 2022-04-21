ee3f4490d541cfa71cc913bb04776bd588d6b253
patriciabrsk <patriciabresku@gmail.com>
2022-04-21 16:52:34 +0200

    css

--- index.php
+++ index.php
@@ -2,35 +2,21 @@
     $faqs = [
         [
             'question' => "How are you implementing the recent Court of Justice of the European Union (CJEU) decision on the right to be forgotten?",
-            'answer' => "The recent ruling by the Court of Justice of the European Union has profound consequences for search engines in Europe. 
-            The court found that certain users have the right to ask search engines like Google to remove results for queries that include the person's name. 
-            To qualify, the results shown would need to be inadequate, irrelevant, no longer relevant, or excessive.
+            'answer' => "The recent ruling by the Court of Justice of the European Union has profound consequences for search engines in Europe. The court found that certain users have the right to ask search engines like Google to remove results for queries that include the person's name. To qualify, the results shown would need to be inadequate, irrelevant, no longer relevant, or excessive.
 
-            Since this ruling was published on 13 May 2014, we've been working round the clock to comply. 
-            This is a complicated process because we need to assess each individual request and balance the rights of the individual to control his or her personal data with public's right to know and distribute information.
+            Since this ruling was published on 13 May 2014, we've been working round the clock to comply. This is a complicated process because we need to assess each individual request and balance the rights of the individual to control his or her personal data with public's right to know and distribute information.
             
-            If you have a removal request, please fill out this webform. You'll receive an automatic reply confirming that we have received your request. 
-            We will then assess your case—please note this may take some time because we have already received many such requests. 
-            In evaluating your request, we will look at whether the results include outdated information about your private life. 
-            We'll also look at whether there's a public interest in the information remaining in our search results—for example, if it relates to financial scams, professional malpractice, criminal convictions or your public conduct as a government official (elected or unelected). 
-            These are difficult judgements and as a private organization, we may not be in a good position to decide on your case. If you disagree with our decision you can contact your local DPA.
+            If you have a removal request, please fill out this webform. You'll receive an automatic reply confirming that we have received your request. We will then assess your case—please note this may take some time because we have already received many such requests. In evaluating your request, we will look at whether the results include outdated information about your private life. We'll also look at whether there's a public interest in the information remaining in our search results—for example, if it relates to financial scams, professional malpractice, criminal convictions or your public conduct as a government official (elected or unelected). These are difficult judgements and as a private organization, we may not be in a good position to decide on your case. If you disagree with our decision you can contact your local DPA.
             
-            We look forward to working closely with data protection authorities and others over the coming months as we refine our approach. 
-            The CJEU's ruling constitutes a significant change for search engines. 
-            While we are concerned about its impact, we also believe it's important to respect the Court's judgment and are working hard to devise a process that complies with the law.
+            We look forward to working closely with data protection authorities and others over the coming months as we refine our approach. The CJEU's ruling constitutes a significant change for search engines. While we are concerned about its impact, we also believe it's important to respect the Court's judgment and are working hard to devise a process that complies with the law.
             
-            When you search for a name, you may see a notice that says that results may have been modified in accordance with data protection law in Europe. 
-            We’re showing this notice in Europe when a user searches for most names, not just pages that have been affected by a removal."
+            When you search for a name, you may see a notice that says that results may have been modified in accordance with data protection law in Europe. We’re showing this notice in Europe when a user searches for most names, not just pages that have been affected by a removal."
         ],
         [
             'question' => "How does Google protect my privacy and keep my information secure?",
-            'answer' => "We know security and privacy are important to you – and they are important to us, too. 
-            We make it a priority to provide strong security and give you confidence that your information is safe and accessible when you need it.
+            'answer' => "We know security and privacy are important to you – and they are important to us, too. We make it a priority to provide strong security and give you confidence that your information is safe and accessible when you need it.
 
-            We’re constantly working to ensure strong security, protect your privacy, and make Google even more effective and efficient for you. 
-            We spend hundreds of millions of dollars every year on security, and employ world-renowned experts in data security to keep your information safe. 
-            We also built easy-to-use privacy and security tools like Google Dashboard, 2-step verification and Ads Settings. 
-            So when it comes to the information you share with Google, you’re in control.
+            We’re constantly working to ensure strong security, protect your privacy, and make Google even more effective and efficient for you. We spend hundreds of millions of dollars every year on security, and employ world-renowned experts in data security to keep your information safe. We also built easy-to-use privacy and security tools like Google Dashboard, 2-step verification and Ads Settings. So when it comes to the information you share with Google, you’re in control.
             
             You can learn more about safety and security online, including how to protect yourself and your family online, at the Google Safety Center.
             
@@ -40,9 +26,8 @@
             'question' => "Why is my account associated with a country?",
             'answer' => "Your account is associated with a country (or territory) in the Terms of Service so that we can determine two things:
 
-            The Google affiliate that provides the services, that processes your information, and that is responsible for complying with applicable privacy laws. 
-            Generally, Google offers its consumer services through either of two companies:
-            Google Ireland Limited, if you’re located in the European Economic Area (EU countries plus Iceland, Liechtenstein, and Norway) or Switzerland
+            1. The Google affiliate that provides the services, that processes your information, and that is responsible for complying with applicable privacy laws. 
+            Generally, Google offers its consumer services through either of two companies: Google Ireland Limited, if you’re located in the European Economic Area (EU countries plus Iceland, Liechtenstein, and Norway) or Switzerland
             Google LLC, based in the United States, for the rest of the world
             The version of the terms that govern our relationship, which can vary depending on local laws
             Keep in mind that Google services are essentially the same regardless of the affiliate that provides the services or your country association.
@@ -84,18 +69,28 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+
     <title>Google FAQ</title>
 </head>
 <body>
-    <header></header>
-    <main class="container d-flex justify-content-center">
-        <ul class="list-unstyled">
+    <header>
+        <nav class="navbar border-bottom">
+            <div class="container-fluid">
+                <a href="#">
+                    <img src="google.png" alt="Google logo">
+                </a>
+                
+            </div>
+        </nav>
+    </header>
+    <main class="container d-flex justify-content-center px-5">
+        <ul class="list-unstyled p-5">
             <?php foreach ($faqs as $faq) {?>
-                <li class="pb-4">
-                    <span class="fw-bold fs-3"><?php echo $faq['question'] . '<br>'; ?></span>
-                    <?php echo $faq['answer']; ?>
+                <li class="pb-5 px-5">
+                    <span class="fw-bold fs-3 mb-5"><?php echo nl2br($faq['question']) . '<br>'; ?></span>
+                    <?php echo nl2br($faq['answer']); ?>
                 </li>
             <?php }?> 
         </ul>
