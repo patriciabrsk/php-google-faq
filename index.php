79493ac1b21de087932f74627dc9e37ccb16b9ea
patriciabrsk <patriciabresku@gmail.com>
2022-04-21 17:56:59 +0200

    fixed paragraphs

--- index.php
+++ index.php
@@ -30,32 +30,20 @@
             Keep in mind that Google services are essentially the same regardless of the affiliate that provides the services or your country association.
             
             <h5>Determining the country associated with your account</h5>
-            When you create a new account, we associate your account with a country based on where you created your Google Account. 
-            For accounts at least a year old, we use the country from which you usually access Google services — typically where you’ve spent the most time in the last year.
+            When you create a new account, we associate your account with a country based on where you created your Google Account. For accounts at least a year old, we use the country from which you usually access Google services — typically where you’ve spent the most time in the last year.
             
             Frequent travel doesn’t generally affect the country associated with your account. If you move to a new country, it can take about a year for your country association to update.
             
-            If the country associated with your account doesn’t correspond to your country of residence, it could be because of a difference between your country of work and residence, because you’ve installed a Virtual Private Network (VPN) to mask your IP address, or because you live close to a territorial border. 
-            Contact us if you think your country association is wrong."
+            If the country associated with your account doesn’t correspond to your country of residence, it could be because of a difference between your country of work and residence, because you’ve installed a Virtual Private Network (VPN) to mask your IP address, or because you live close to a territorial border. <a class='text-decoration-none' href='#'>Contact us</a> if you think your country association is wrong."
         ],
         [
             'question' => "How can I remove information about myself from Google's search results?",
-            'answer' => "Google search results are a reflection of the content publicly available on the web. 
-            Search engines can't remove content directly from websites, so removing search results from Google wouldn't remove the content from the web. 
-            If you want to remove something from the web, you should contact the webmaster of the site the content is posted on and ask him or her to make a change. 
-            Additionally, if under European data protection law, you would like to request removal of certain information about you that appears in Google's search results, please click here. 
-            Once the content has been removed and Google has noted the update, the information will no longer appear in Google's search results. 
-            If you have an urgent removal request, you can also visit our help page for more information."
+            'answer' => "Google search results are a reflection of the content publicly available on the web. Search engines can't remove content directly from websites, so removing search results from Google wouldn't remove the content from the web. If you want to remove something from the web, you should contact the webmaster of the site the content is posted on and ask him or her to make a change. Additionally, if under European data protection law, you would like to request removal of certain information about you that appears in Google's search results, please click here. Once the content has been removed and Google has noted the update, the information will no longer appear in Google's search results. If you have an urgent removal request, you can also <a class='text-decoration-none' href='#'>visit our help page for more information</a>."
         ],
         [
             'question' => "Are my search queries sent to websites when I click on Google Search results?",
-            'answer' => "<p>In some cases, yes. When you click on a search result in Google Search, your web browser also may send the Internet address, or URL, of the search results page to the destination webpage as the Referrer URL. 
-            The URL of the search results page may sometimes contain the search query you entered. 
-            If you are using SSL Search (Google’s encrypted search functionality), under most circumstances, your search terms will not be sent as part of the URL in the Referrer URL. 
-            There are some exceptions to this behavior, such as if you are using some less popular browsers. 
-            More information on SSL Search can be found here. 
-            Search queries or information contained in the Referrer URL may be available via Google Analytics or an application programming interface (API). 
-            In addition, advertisers may receive information relating to the exact keywords that triggered an ad click.</p>"
+            'answer' => "<p>In some cases, yes. When you click on a search result in Google Search, your web browser also may send the Internet address, or URL, of the search results page to the destination webpage as the Referrer URL. The URL of the search results page may sometimes contain the search query you entered. If you are using SSL Search (Google’s encrypted search functionality), under most circumstances, your search terms will not be sent as part of the URL in the Referrer URL. 
+            There are some exceptions to this behavior, such as if you are using some less popular browsers. More information on SSL Search can be found here. Search queries or information contained in the Referrer URL may be available via Google Analytics or an application programming interface (API). In addition, advertisers may receive information relating to the exact keywords that triggered an ad click.</p>"
         ]
     ];
 ?>
@@ -75,13 +63,13 @@
     <header>
         <nav class="border-bottom px-3">
             <div class="container-fluid p-2">
-                <div class="logo-box d-flex">
+                <div class="logo-box d-flex align-items-center">
                     <a href="#" class="me-2">
                         <img src="google.png" alt="Google logo">
                     </a>
-                    <p class="text-secondary fs-5">Privacy & Terms</p>
+                    <p class="text-secondary m-0">Privacy & Terms</p>
                 </div>
-                <div class="navbar-box d-flex">
+                <div class="navbar-box d-flex fs-6">
                     <a class="text-decoration-none text-secondary me-5" href="#">Overview</a>
                     <a class="text-decoration-none text-secondary me-5" href="#">Privacy Policy</a>
                     <a class="text-decoration-none text-secondary me-5" href="#">Terms of Service</a>
